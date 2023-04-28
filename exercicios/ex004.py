d38d230ac0e83af16ab70e76d145c32972592183
Felipe Santos Damasceno <126077568+Kenway9503@users.noreply.github.com>
2023-04-28 12:10:57 -0300

    Create ex004.py

---
+++ exercicios/ex004.py
@@ -0,0 +1,21 @@
+#5+3*2 == 11
+#3*5+4**2 == 31
+#3*(5+4)**2 == 243
+
+# () 
+# ** 
+# * / // % 
+# + - 
+
+#nome = input('Qual é seu nome? ')
+#print(f'Prazer em te conhecer {nome:=^20}!')
+
+n1 = int(input('Um valor '))
+n2 = int(input('Outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print(f'A soma é {s}, o produto é {m} e a divisão é {d:.3f}', end=' ')
+print(f'Divisão inteira {di} e potência {e}')
