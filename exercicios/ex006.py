ade43909044d70035a1f63895419eb7e3ed29f8e
Felipe Santos Damasceno <126077568+Kenway9503@users.noreply.github.com>
2023-05-01 10:51:39 -0300

    Create ex006.py

---
+++ exercicios/ex006.py
@@ -0,0 +1,7 @@
+n1 = int(input('Diga um número: '))
+m = n1 * 2
+t = n1 * 3
+r = n1 ** (1/2)
+print(f'O dobro de {n1} vale {m}.')
+print(f'o triplo de {n1} vale {t}.')
+print(f'A raiz quadrada de {n1} é igual a {r:.3}.')
