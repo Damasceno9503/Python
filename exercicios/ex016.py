c392c99d4dd8dac93f7410ca10696c0df0f83667
Felipe Santos Damasceno <126077568+Kenway9503@users.noreply.github.com>
2023-05-09 12:16:42 -0300

    novos ex

---
+++ exercicios/ex016.py
@@ -0,0 +1,7 @@
+from math import trunc
+num = float(input('Digite um numero com pontos flutuantes: '))
+por = trunc(num)
+print(f'O valor digitado foi {num} e a sua porção inteira é {por}')
+
+'''num = float(input('Digite um número: '))
+print(f'O valor digitado foi {num} e a sua porção inteira é {int}')'''
