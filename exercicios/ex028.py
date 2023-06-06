2b13c425b2ac74c2a476d72ce257b906599cd70c
Felipe Santos Damasceno <126077568+Kenway9503@users.noreply.github.com>
2023-06-06 11:24:07 -0300

    Update ex028.py

--- exercicios/ex028.py
+++ exercicios/ex028.py
@@ -1,9 +1,12 @@
 from random import randint
+from time import sleep
 print('-=-' *20)
 str(print('Vou pensar em um número entre 0 e 5. tente adivinhar...'))
 print('-=-' *20)
 jogador = int(input('Em que número eu pensei? ')) #numero q o jogador pensa
 computador = randint(0, 5) #numero q o computador pensa
+print('PROCESSANDO...')
+sleep(2)
 if jogador == computador:
     print('PARABÉNS! voce ganhou de mim!')
     print('=' *60)
