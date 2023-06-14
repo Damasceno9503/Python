6e4f15f8b0285db366ca49560fe15397ed8e4209
Felipe Santos Damasceno <126077568+Kenway9503@users.noreply.github.com>
2023-06-14 10:51:09 -0300

    Update ex028.py

--- exercicios/ex028.py
+++ exercicios/ex028.py
@@ -1,15 +1,19 @@
 from random import randint
 from time import sleep
-print('-=-' *20)
+cores = {'vermelho' : '\033[31m',
+         'amarelo' : '\033[33m',
+         'limpa' : '\033[m',
+         'verde' : '\033[32m'}
+print(f'{cores["amarelo"]}-=-{cores["limpa"]}' *20)
 str(print('Vou pensar em um número entre 0 e 5. tente adivinhar...'))
-print('-=-' *20)
+print(f'{cores["amarelo"]}-=-{cores["limpa"]}' *20)
 jogador = int(input('Em que número eu pensei? ')) #numero q o jogador pensa
 computador = randint(0, 5) #numero q o computador pensa
-print('PROCESSANDO...')
+print(f'{cores["amarelo"]}PROCESSANDO...{cores["limpa"]}')
 sleep(2)
 if jogador == computador:
-    print('PARABÉNS! voce ganhou de mim!')
+    print(f'{cores["verde"]}PARABÉNS! voce ganhou de mim!{cores["limpa"]}')
     print('=' *60)
 else:
-    print(f'EU GANHEI! eu pensei no número {computador} e não no {jogador}!')
-    print('=' *60)
+    print(f'{cores["vermelho"]}EU GANHEI! eu pensei no número {computador} e não no {jogador}!{cores["limpa"]}')
+    print(f'{cores["amarelo"]}={cores["limpa"]}' *60)
